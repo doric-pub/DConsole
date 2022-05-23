55b1ff6bb3c55060a55e2694480de6cc5736d24b
pengfei.zhou <pengfeizhou@foxmail.com>
2022-05-23 14:06:54 +0800

    Init commit

---
+++ iOS/Classes/dConsoleLibrary.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+#import <DoricCore/Doric.h>
+
+@interface dConsoleLibrary : DoricLibrary
+@end
---
+++ iOS/Classes/dConsoleLibrary.m
@@ -0,0 +1,12 @@
+#import "dConsoleLibrary.h"
+#import "DoricDemoPlugin.h"
+
+@implementation dConsoleLibrary
+- (void)load:(DoricRegistry *)registry {
+    NSString *path = [[NSBundle mainBundle] bundlePath];
+    NSString *fullPath = [path stringByAppendingPathComponent:@"bundle_dconsole.js"];
+    NSString *jsContent = [NSString stringWithContentsOfFile:fullPath encoding:NSUTF8StringEncoding error:nil];
+    [registry registerJSBundle:jsContent withName:@"dconsole"];
+    [registry registerNativePlugin:DoricDemoPlugin.class withName:@"demoPlugin"];
+}
+@end
