807de8fb0b20ffae18a41c37c8772f90b8368179
吴尚昆 <shangkunwu@msn.com>
2022-05-25 15:27:36 +0800

    update StateModel

--- example/src/module/StateModule.tsx
+++ example/src/module/StateModule.tsx
@@ -1,7 +1,11 @@
-import { Color, Group } from "doric";
+import { Color, Group, log, Panel, Setter, ViewHolder, VMPanel } from "doric";
 import { DCModule } from "./dcModule";
+import { ClassType } from "doric"
 
-type StateModel = {}[];
+type StateModel = {
+    action: string,
+    state: any,
+}[];
 
 export class StateModule extends DCModule<StateModel> {
 
@@ -16,9 +20,39 @@ export class StateModule extends DCModule<StateModel> {
     }
     onAttached(state: StateModel): void {
 
+        // const panel = context.entity as Panel;
+
+        const panel = context.entity as VMPanel<Object, ViewHolder>;
+        log(`onAttached start: ${JSON.stringify(panel.allHeadViews)}`);
+
+        if (panel instanceof VMPanel) {
+            const originShow = (panel as any)["__onShow__"];
+
+            (panel as any)["__onShow__"] = () => {
+                Reflect.apply(originShow, panel, []);
+
+                log(`__onShow__ onAttached: ${panel.getState}`);
+                const originVM = (panel as any)["vm"];
+                log(`__onShow__ originVM: ${originVM}`);
+
+            };
+
+        }
+
+        // const originVM = (panel as any)["vm"];
+        // log(`originVM: ${originVM}`);
+
+
+        // originVM["updateState"] = (setter: any) => {
+        //     // log(`updateState: ${panel.getState}`);
+        // };
+
+
+
+        log(`onAttached end.....`);
+
     }
-
     onBind(state: StateModel): void {
-
+        log(`onBind`);
     }
 }
