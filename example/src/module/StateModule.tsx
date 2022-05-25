a2067ae150e68b3d1864464be454472ff80ae58d
吴尚昆 <shangkunwu@msn.com>
2022-05-25 18:36:17 +0800

    add State Module List UI.

--- example/src/module/StateModule.tsx
+++ example/src/module/StateModule.tsx
@@ -1,13 +1,13 @@
-import { Color, Group, log, Panel, Setter, ViewHolder, VMPanel } from "doric";
+import { Color, createRef, Gravity, Group, jsx, layoutConfig, List, ListItem, log, Stack, Text, VMPanel } from "doric";
 import { DCModule } from "./dcModule";
-import { ClassType } from "doric"
 
 type StateModel = {
-    action: string,
-    state: any,
+    action?: string,
+    value?: any,
 }[];
 
 export class StateModule extends DCModule<StateModel> {
+    listRef = createRef<List>();
 
     title(): string {
         return 'State';
@@ -17,6 +17,11 @@ export class StateModule extends DCModule<StateModel> {
     }
     build(group: Group): void {
         group.backgroundColor = Color.CYAN;
+        <List
+            parent={group} layoutConfig={layoutConfig().most()}
+            backgroundColor={Color.CYAN}
+            ref={this.listRef}
+        />
     }
     onShow(): void {
         log(`StateModule onShow`)
@@ -26,24 +31,49 @@ export class StateModule extends DCModule<StateModel> {
     }
 
     onAttached(state: StateModel): void {
-
-        const panel = context.entity;
-        log(`panel type: ${typeof panel}`);
-
+        this.listRef.current.renderItem = (i) => {
+            return (
+                <ListItem layoutConfig={layoutConfig().mostWidth().fitHeight()}>
+                    <Text
+                        layoutConfig={layoutConfig().mostWidth().fitHeight()}
+                        maxLines={-1}
+                        padding={{ left: 5, top: 5, right: 5, bottom: 5 }}
+                        textAlignment={Gravity.CenterY.left()}
+                        textSize={12}
+                    >
+                        {`${state[i].action}: ${JSON.stringify(state[i].value)}`}
+                    </Text>
+                    <Stack
+                        layoutConfig={layoutConfig().mostWidth().justHeight()}
+                        height={0.5}
+                        backgroundColor={Color.parse("#bdc3c7")}
+                    />
+                </ListItem>
+            ) as ListItem;
+        };
+        const panel = this.context.entity;
+        const self = this;
         if (panel instanceof VMPanel) {
-            // const panel = context.entity as VMPanel<Object, ViewHolder>;
-            log(`StateModule onAttached: ${JSON.stringify(panel.getState())}`);
-
-            // const originShow = (panel as any)["__onShow__"];
-            // (panel as any)["__onShow__"] = () => {
-            //     Reflect.apply(originShow, panel, []);
-            //     log(`StateModule __onShow__`);
-            // };
+            const vm = panel.getViewModel();
+            if (vm && Reflect.has(vm, 'onAttached')) {
+                self.updateState((state) => {
+                    state.push({ action: '默认数据', value: panel.getState() });
+                })
+                const originUpdateState = (vm as any)["updateState"];
+                (vm as any)["updateState"] = (setter: any) => {
+                    Reflect.apply(originUpdateState, vm, [setter]);
+                    // log(`StateModule updateState2222: ${JSON.stringify(vm.getState())}`);
+                    self.updateState((state) => {
+                        state.push({ action: 'updateState', value: vm.getState() });
+                    })
+                }
+            }
         }
-        log(`onAttached end.....`);
     }
 
     onBind(state: StateModel): void {
-        log(`onBind`);
+        // log(`StateModule onBind: ${JSON.stringify(state)}`);
+        this.listRef.current.itemCount = state.length;
+        this.listRef.current.scrolledPosition = state.length - 1;
     }
 }
