bfe3d1b5289faa7487489ea96e4b90c16d72d660
吴尚昆 <shangkunwu@msn.com>
2022-05-26 21:22:26 +0800

    use immutable.js

--- example/src/module/StateModule.tsx
+++ example/src/module/StateModule.tsx
@@ -1,39 +1,44 @@
-import { Color, createRef, Gravity, Group, jsx, layoutConfig, List, ListItem, log, Stack, Text, VMPanel } from "doric";
+import { Color, createRef, Gravity, Group, jsx, layoutConfig, List, ListItem, Stack, Text, VMPanel } from "doric";
 import { DCModule } from "./dcModule";
+import { Map } from 'immutable';
+import Immutable from "immutable";
 
-type StateModel = {
-    action?: string,
-    value?: any,
-}[];
+// type StateModel = {
+//     action?: string,
+//     value?: any,
+// }[];
+
+type StateModel = Map<string, any>[];
 
 export class StateModule extends DCModule<StateModel> {
+    history: Map<string, any>[] = [];
     listRef = createRef<List>();
 
     title(): string {
         return 'State';
     }
+
     state(): StateModel {
         return [];
     }
+
     build(group: Group): void {
         group.backgroundColor = Color.CYAN;
         <List
             parent={group} layoutConfig={layoutConfig().most()}
-            backgroundColor={Color.CYAN}
+            backgroundColor={Color.WHITE}
             ref={this.listRef}
         />
-    }
-    onShow(): void {
-        log(`StateModule onShow`)
-    }
-    onHide(): void {
-        log(`StateModule onHide`)
     }
 
     onAttached(state: StateModel): void {
         this.listRef.current.renderItem = (i) => {
+            const model = this.history[i];
+            if (!model) return <ListItem /> as ListItem;
             return (
-                <ListItem layoutConfig={layoutConfig().mostWidth().fitHeight()}>
+                <ListItem
+                    layoutConfig={layoutConfig().mostWidth().fitHeight()}
+                >
                     <Text
                         layoutConfig={layoutConfig().mostWidth().fitHeight()}
                         maxLines={-1}
@@ -41,7 +46,7 @@ export class StateModule extends DCModule<StateModel> {
                         textAlignment={Gravity.CenterY.left()}
                         textSize={12}
                     >
-                        {`${state[i].action}: ${JSON.stringify(state[i].value)}`}
+                        {`${model.get('action')}: ${JSON.stringify(model.get('value'))}`}
                     </Text>
                     <Stack
                         layoutConfig={layoutConfig().mostWidth().justHeight()}
@@ -56,24 +61,33 @@ export class StateModule extends DCModule<StateModel> {
         if (panel instanceof VMPanel) {
             const vm = panel.getViewModel();
             if (vm && Reflect.has(vm, 'onAttached')) {
-                self.updateState((state) => {
-                    state.push({ action: '默认数据', value: panel.getState() });
+                self.updateState((s) => {
+                    self.saveStateToHistory(panel.getState(), s, '默认数据');
                 })
                 const originUpdateState = (vm as any)["updateState"];
                 (vm as any)["updateState"] = (setter: any) => {
                     Reflect.apply(originUpdateState, vm, [setter]);
-                    // log(`StateModule updateState2222: ${JSON.stringify(vm.getState())}`);
-                    self.updateState((state) => {
-                        state.push({ action: 'updateState', value: vm.getState() });
+                    self.updateState((s) => {
+                        self.saveStateToHistory(vm.getState(), s, 'updateState');
                     })
                 }
             }
         }
     }
 
+    saveStateToHistory(stateObj: Object, s: StateModel, action: string) {
+        // const string = JSON.stringify(stateObj);
+        // const obj = JSON.parse(string);
+        const immutableMap = Immutable.fromJS(stateObj);
+        const data = Immutable.Map({ action: action, value: immutableMap });
+        this.history.push(data);
+        s.push(data);
+    }
+
     onBind(state: StateModel): void {
-        // log(`StateModule onBind: ${JSON.stringify(state)}`);
-        this.listRef.current.itemCount = state.length;
-        this.listRef.current.scrolledPosition = state.length - 1;
+        this.listRef.current.itemCount = this.history.length;
+        if (this.history.length > 0) {
+            this.listRef.current.scrolledPosition = this.history.length - 1;
+        }
     }
 }
